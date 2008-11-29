b8dd8080de29a377ebccbedd4f00f9c80353266c
root <root@benko.login.cx>
2008-11-29 02:02:52 +0000

    add quite some functionality

--- getfromxml.py
+++ getfromxml.py
@@ -6,21 +6,33 @@ import string
 import gpx2list
 import tktogpx2
 import flickrupload
+import urllib
+import ConfigParser
 
 basepath='/srv/trackdata/bydate/'
 
+def getcredentials(credentialfile):
+    config=ConfigParser.ConfigParser()
+    open(credentialfile)
+    config.read(credentialfile)
+    flickrapi_key=config.get("flickrcredentials","api_key")
+    flickrapi_secret=config.get("flickrcredentials","api_secret")
+    wteapi_key=config.get("worldtimeengine","api_key")
+    return flickrapi_key,flickrapi_secret,wteapi_key
+
+
 def parsexml(xmlfile):
     tree = etree.fromstring(file(basepath+xmlfile, "r").read())
-    topic =  etree.tostring(tree.xpath('//topic')[0]).split("<topic>")[1].split("</topic>")[0].replace("\n","").replace("&gt;",">").replace("&lt;","<")
-    logtext =  etree.tostring(tree.xpath('//logtext')[0]).split("<logtext>")[1].split("</logtext>")[0].replace("\n","").replace("&gt;",">").replace("&lt;","<")
-    filepath =  etree.tostring(tree.xpath('//filepath')[0]).split("<filepath>")[1].split("</filepath>")[0].replace("\n","")
-    trackfile =  etree.tostring(tree.xpath('//trackfile')[0]).split("<trackfile>")[1].split("</trackfile>")[0].replace("\n","")
-    photoset =  etree.tostring(tree.xpath('//photoset')[0]).split("<photoset>")[1].split("</photoset>")[0].replace("\n","")
+    topic =  (tree.xpath('//topic')[0]).text.replace("&gt;",">").replace("&lt;","<")
+    logtext =  (tree.xpath('//logtext')[0]).text.replace("&gt;",">").replace("&lt;","<")
+    filepath =  (tree.xpath('//filepath')[0]).text
+    trackfile =  (tree.xpath('//trackfile')[0]).text
+    photoset =  (tree.xpath('//photoset')[0]).text
     i=1
     imglist=dict()
     while i < 100:
 	try:
-	    imglist[i]=etree.tostring(tree.xpath('//img'+str(i))[0]).split("<img"+str(i)+">")[1].split("</img"+str(i)+">")[0].replace("\n","")
+	    imglist[i]=(tree.xpath('//img'+str(i))[0]).text
 	    i=i+1
 	except IndexError:
 	    i=i+1
@@ -28,15 +40,84 @@ def parsexml(xmlfile):
     return topic,logtext,filepath,trackfile,photoset,imglist		
 
 
-def gpx2database(gpxfile):
+def query_wte(wteapi_key,lat,long):
+    f = urllib.urlopen("http://worldtimeengine.com/api/"+wteapi_key+"/"+str(lat)+"/"+str(long))
+    geodetails=f.read()
+    f.close()
+    return geodetails
+
+
+def gpx2database(gpxfile,wteapi_key):
+######################### replace this shit by worldtimeengine-query when finished #############
+    geodetailsfirst=etree.fromstring('''<?xml version="1.0" encoding="UTF-8" ?>
+<timezone xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://worldtimeengine.com/timezone.xsd">
+    <version>1.1</version>
+    <location>
+        <region>Taiwan</region>
+        <latitude>25.0684016</latitude>
+        <longitude>121.6382592</longitude>
+    </location>
+    <time>
+        <utc>2008-11-28 21:35:12</utc>
+        <local>2008-11-29 05:35:12</local>
+        <zone>
+            <hasDST>false</hasDST>
+            <current>
+                <abbreviation>CST</abbreviation>
+                <description>Chinese Standard Time</description>
+                <utcoffset>-8:00</utcoffset>
+            </current>
+        </zone>
+    </time>
+</timezone>''')
+
+    geodetailslast=etree.fromstring('''<?xml version="1.0" encoding="UTF-8" ?>
+<timezone xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://worldtimeengine.com/timezone.xsd">
+    <version>1.1</version>
+    <location>
+        <region>Taiwan</region>
+        <latitude>24.9993056</latitude>
+        <longitude>121.49152</longitude>
+    </location>
+    <time>
+        <utc>2008-11-28 21:35:12</utc>
+        <local>2008-11-29 05:35:12</local>
+        <zone>
+            <hasDST>false</hasDST>
+            <current>
+                <abbreviation>CST</abbreviation>
+                <description>Chinese Standard Time</description>
+                <utcoffset>-8:00</utcoffset>
+            </current>
+        </zone>
+    </time>
+</timezone>''')
+    ################################################################################
+
     trkptlist=gpx2list.trkptlist(gpxfile)
-    for lat,long in trkptlist:
-	print lat
-	print long    
+    lat,long=trkptlist[0] #first point in the track
+    #geodetailsfirst=query_wte(wteapi_key,lat,long)
+    lat,long=trkptlist[-1] #last point in the track
+    #geodetailslast=query_wte(wteapi_key,lat,long)
+    
+    if (geodetailsfirst.xpath('//utcoffset')[0]).text == (geodetailslast.xpath('//utcoffset')[0]).text:
+	utcoffset=(geodetailsfirst.xpath('//utcoffset')[0]).text
+	tz_abbreviation=(geodetailsfirst.xpath('//abbreviation')[0]).text	
+	tz_description=(geodetailsfirst.xpath('//description')[0]).text
+	tz_region=(geodetailsfirst.xpath('//region')[0]).text	
+	print tz_abbreviation,tz_description,tz_region	
+    else:
+	print 'We need to check this tracklist in more detail, as there was a shift in the timezone'
+	print 'between the first and the last trackpoint'
+	i=False
+	#function to check the tracklist toroughly goes here
+    
+    
+    
+
+	    
 
 def geotag(imagepath,gpxfile):#geotag the pictures in imagepath with data from gpxfile
-    print imagepath
-    print gpxfile
     if os.system("/usr/bin/perl /root/scripts/gpsPhoto.pl --dir "+imagepath+" --gpsfile "+gpxfile+" --timeoffset 0 --overwrite-geotagged > /var/log/poab/geotag.log 2>&1") == 0:
 	pass
     else:
@@ -46,15 +127,13 @@ def geotag(imagepath,gpxfile):#geotag the pictures in imagepath with data from g
 def img2database(flickrphotoid,flickrimgdetails):
    print flickrphotoid 
 		
-def img2flickr(imagepath,imglist,gpxfile):
+def img2flickr(imagepath,imglist,gpxfile,flickrapi_key,flickrapi_secret):
     filetypes=('.png','.jpg','.jpeg','.gif')
     for image in os.listdir(imagepath):
 	if image.lower().endswith(filetypes):
 	    #get the exif-geo-info with jhead
 	    geoinfo=os.popen("/usr/bin/jhead -exifmap "+imagepath+image+"|/bin/grep Spec|/usr/bin/awk {'print $5 $7'}").readlines()
 	    latitude,longitude=geoinfo[0].strip('\n').split(',',1)
-	    print latitude
-	    print longitude
 	    try:
 		flickrphotoid=flickrupload.imgupload(imagepath+image,'testtitle','testdescription','blabla "sadfa asfaf" asfd')
 		flickrimgdetails = flickrupload.getimginfo(flickrphotoid)
@@ -65,6 +144,7 @@ def img2flickr(imagepath,imglist,gpxfile):
 
 
 def main(basepath):
+    flickrapi_key,flickrapi_secret,wteapi_key=getcredentials('/root/scripts/credentials.ini')
     for xmlfile in os.listdir(basepath):
 	if xmlfile.lower().endswith('.xml'):
 	    xmlcontent=parsexml(xmlfile)
@@ -83,9 +163,9 @@ def main(basepath):
 	    except IOError:
 		print 'trackfile missing!'
 		gpxfile=None
-	    gpx2database(gpxfile)
+	    gpx2database(gpxfile,wteapi_key)
 	    geotag(imagepath,gpxfile)
-	    img2flickr(imagepath,imglist,gpxfile)
+	    #img2flickr(imagepath,imglist,gpxfile,flickrapi_key,flickrapi_secret)
 
 main(basepath)
 
